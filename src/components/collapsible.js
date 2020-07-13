10ec285c04de4f0eefb962120363f836ea11f0fc
Sam Goddard <hello@samgoddard.co.uk>
2020-07-13 14:59:36 +0100

    ironed out some bugs, updated work page hero template

--- src/components/collapsible.js
+++ src/components/collapsible.js
@@ -69,9 +69,9 @@ const Collapsible = ({ children, heading, index, textLarge }) => {
 
   return (
     <button className="Collapse w-full group strike" onClick={() => handleClick()}>
-      <div className="flex flex-wrap items-center border-b border-current w-full py-3 md:py-6 xl:py-8">
+      <div className="flex flex-wrap items-start border-b border-current w-full py-3 md:py-6 xl:py-8">
         <span className="block text-xs mr-3 w-full md:w-auto text-left mb-2 md:mb-0">{ index }</span>
-        <span className={textLarge ? `block text-xl md:text-2xl xl:text-3xl text-left font-display leading-extratight mb-0 pb-0 md:-mb-3 strike__inner strike__inner--small w-auto` : `block text-xl md:text-2xl text-left font-display leading-extratight mb-0 pb-0 md:-mb-2 w-auto strike__inner strike__inner--small`}>{ heading }</span>
+        <span className={textLarge ? `block text-xl md:text-2xl xl:text-3xl text-left font-display leading-extratight mb-0 pb-0 md:-mb-3 strike__inner strike__inner--small w-9/12 md:w-auto` : `block text-xl md:text-2xl text-left font-display leading-extratight mb-0 pb-0 md:-mb-2 w-9/12 md:w-auto strike__inner strike__inner--small`}>{ heading }</span>
         <span className="block ml-auto pl-4 transform origin-center rotate-0">
           <svg xmlns="http://www.w3.org/2000/svg" className={state.clicked ? `w-4 md:w-6 transform rotate-90 transition duration-500 ease-in-out` : `transition duration-500 ease-in-out w-4 md:w-6 transform -rotate-90`} viewBox="0 0 27.197 23.217"><g data-name="Group 116" fill="none" stroke="currentColor" strokeWidth="2"><path data-name="Path 1" d="M12.314 22.51l-10.9-10.9 10.9-10.9"/><path data-name="Path 2" d="M1.414 11.609h25.783"/></g></svg>
         </span>
