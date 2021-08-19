2fd69aef740211d578d728e851e902697378e6f2
Sam Goddard <hello@samgoddard.co.uk>
2021-08-19 09:57:26 +0100

    GA added

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,26 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-BDVN8XQRWZ", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      
+        pluginConfig: {
+          head: true,
+        },
+      }
+    },
+    {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: `${process.env.DATOCMS_KEY}`,
